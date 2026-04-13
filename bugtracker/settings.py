1afa4c6d118b9c3b3f6a1c0f9158d94b22396a8b
rani <raniy5426@gmail.com>
2026-04-14 02:20:31 +0530

    fixed requirements

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-16eklj69%dc!@ln3e+2za7homi+x6z3e1@)=5szh7&qkau%tt=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
